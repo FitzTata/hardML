460a044906ead5965f2036c610fe70bd74785504
Alexandr <90062563+FitzTata@users.noreply.github.com>
2024-08-14 12:10:19 +0300

    Update rm_l2.py

--- Matching/l2/rm_l2.py
+++ Matching/l2/rm_l2.py
@@ -24,6 +24,16 @@ def num_swapped_pairs(ys_true: Tensor, ys_pred: Tensor) -> int:
 
     return swapped_count
 
+def test_num_swapped_pairs():
+    tests = ((Tensor([5, 4, 3, 2, 1]), Tensor([0.9, 0.8, 0.7, 0.6, 0.5]), 0),
+             (Tensor([4, 5, 3, 2, 1]), Tensor([0.9, 0.8, 0.7, 0.6, 0.5]), 1),
+             (Tensor([1, 2, 3, 4, 5]), Tensor([0.9, 0.8, 0.7, 0.6, 0.5]), 10),
+             (Tensor([0, 2, 2]), Tensor([0.1, 0.4, 0.5]), 0)
+            )
+    for test in tests:
+        assert num_swapped_pairs(test[0], test[1]) == test[2], f'{num_swapped_pairs(test[0], test[1])} != {test[2]}'
+    print('tests ok')
+    
 
 def compute_gain(y_value: float, gain_scheme: str) -> float:
     if gain_scheme == 'const':
@@ -40,7 +50,15 @@ def dcg(ys_true: Tensor, ys_pred: Tensor, gain_scheme: str) -> float:
 
     sorted_ys_true = sort_true_by_pred(ys_true.double(), ys_pred)
     return sum([compute_gain(val, gain_scheme) / log2(idx + 2) for idx, val in enumerate(sorted_ys_true)])
-
+    
+def test_dcg():
+    tests = ((Tensor([3, 2, 3, 0, 1, 2]), Tensor([0.9, 0.8, 0.7, 0.6, 0.5, 0.55]), 'exp2', 13.909554869526021),
+             (Tensor([3, 2, 3, 0, 1, 2]), Tensor([0.9, 0.8, 0.7, 0.6, 0.5, 0.55]), 'const', 6.891772308720021)
+            )
+    for test in tests:
+        assert dcg(test[0], test[1], test[2]) == test[3], f'{dcg(test[0], test[1], test[2])} != {test[3]}'
+    print('tests ok')
+    
 
 def ndcg(ys_true: Tensor, ys_pred: Tensor, gain_scheme: str = 'const') -> float:
     dcg_value = dcg(ys_true, ys_pred, gain_scheme)
@@ -50,7 +68,15 @@ def ndcg(ys_true: Tensor, ys_pred: Tensor, gain_scheme: str = 'const') -> float:
     ndcg_value = dcg_value / idcg_value if idcg_value else 0.0
 
     return ndcg_value
-
+    
+def test_ndcg():
+    tests = ((Tensor([3, 2, 3, 0, 1, 2]), Tensor([0.9, 0.8, 0.9, 0.4, 0.5, 0.8]), 'exp2', 1.0),
+             (Tensor([3, 2, 3, 0, 1, 2]), Tensor([0.9, 0.8, 0.9, 0.4, 0.5, 0.8]), 'const', 1.0)
+            )
+    for test in tests:
+        assert ndcg(test[0], test[1], test[2]) == test[3], f'{ndcg(test[0], test[1], test[2])} != {test[3]}'
+    print('tests ok')
+    
 
 def precission_at_k(ys_true: Tensor, ys_pred: Tensor, k: int) -> float:
     def sort_true_by_pred(ys_true: Tensor, ys_pred: Tensor) -> list:
@@ -62,7 +88,15 @@ def precission_at_k(ys_true: Tensor, ys_pred: Tensor, k: int) -> float:
     precission_k = sum(sorted_ys_true[:k]) / min(sum(sorted_ys_true), k)
 
     return precission_k
-
+    
+def test_precission_at_k():
+    tests = ((Tensor([1, 0, 1, 0, 1, 1]), Tensor([.9, .8, .7, .6, .5, .4]), 3, 0.6666666666666666),
+             (Tensor([1, 0, 1, 0]), Tensor([.9, .8, .7, .6]), 3, 1.0)
+            )
+    for test in tests:
+        assert precission_at_k(test[0], test[1], test[2]) == test[3], f'{precission_at_k(test[0], test[1], test[2])} != {test[3]}'
+    print('tests ok')
+    
 
 def reciprocal_rank(ys_true: Tensor, ys_pred: Tensor) -> float:
     try:
@@ -74,10 +108,22 @@ def reciprocal_rank(ys_true: Tensor, ys_pred: Tensor) -> float:
     min_idx = ys_pred_sorted.index(target) + 1
 
     return 1 / min_idx
-
+    
+def test_reciprocal_rank():
+    tests = ((Tensor([1, 0, 0, 0, 0, 0]), Tensor([.9, .8, .7, .6, .5, .4]), 1.0),
+             (Tensor([0, 0, 0, 1]), Tensor([.9, .8, .7, .6]), 0.25),
+             (Tensor([0, 0, 0, 0]), Tensor([.9, .8, .7, .6]), 0.0),
+             (Tensor([0, 0, 0, 1]), Tensor([.6, .8, .9, .7]), 0.3333333333333333)
+            )
+              
+    for test in tests:
+        assert reciprocal_rank(test[0], test[1]) == test[2], f'{reciprocal_rank(test[0], test[1])} != {test[2]}'
+    print('tests ok')
+    
 
 def p_found(ys_true: Tensor, ys_pred: Tensor, p_break: float = 0.15) -> float:
     def sort_true_by_pred(ys_true: Tensor, ys_pred: Tensor) -> list:
+        '''такая структура, чтобы проходили юниттесты на платформе'''
         _, indices = sort(ys_pred, descending=True)
         ys_true_sorted = ys_true[indices]
         return ys_true_sorted.tolist()
@@ -92,9 +138,22 @@ def p_found(ys_true: Tensor, ys_pred: Tensor, p_break: float = 0.15) -> float:
 
     return p_found_val
 
+def test_p_found():
+    tests = ((Tensor([0., 0., 0., 1., 0., 0., 0., 0., 1., 0., 0., 1., 0., 0., 1., 1., 1., 0.,
+        0., 1., 1., 1., 1., 1.]), Tensor([0.0577, 0.0117, 0.7932, 0.2888, 0.0691, 0.9759, 0.4000, 0.3519, 0.3288,
+        0.4549, 0.0026, 0.2685, 0.6604, 0.9075, 0.7337, 0.4975, 0.5701, 0.0463,
+        0.5535, 0.0775, 0.4004, 0.2248, 0.3393, 0.7370]), 0.6141249999999999),
+             (Tensor([1.0]), Tensor([0.5]), 1.0),
+            )
+              
+    for test in tests:
+        assert p_found(test[0], test[1]) == test[2], f'{p_found(test[0], test[1])} != {test[2]}'
+    print('tests ok')
+
 
 def average_precision(ys_true: Tensor, ys_pred: Tensor) -> float:
     def sort_true_by_pred(ys_true: Tensor, ys_pred: Tensor) -> list:
+         '''такая структура, чтобы проходили юниттесты на платформе'''
         _, indices = sort(ys_pred, descending=True)
         ys_true_sorted = ys_true[indices]
         return ys_true_sorted.tolist()
@@ -111,4 +170,14 @@ def average_precision(ys_true: Tensor, ys_pred: Tensor) -> float:
     if n_relevant == 0:
         return 0.0
 
+def test_average_precision():
+    tests = ((Tensor([1, 0, 1, 0, 0, 0]), Tensor([.9, .8, .7, .6, .5, .4]), 0.8333333333333333),
+             (Tensor([0, 0, 0, 1]), Tensor([.9, .8, .7, .6]), 0.25),
+             (Tensor([0, 0, 0, 0]), Tensor([.9, .8, .7, .6]), 0.0),
+             (Tensor([0, 0, 0, 1]), Tensor([.6, .8, .9, .7]), 0.3333333333333333)
+            )
+              
+    for test in tests:
+        assert average_precision(test[0], test[1]) == test[2], f'{average_precision(test[0], test[1])} != {test[2]}'
+    print('tests ok')
     return cum_precision / n_relevant
